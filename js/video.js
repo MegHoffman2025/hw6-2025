35b70c606e5c7611cda27ae3e7db3294501dc3ab
Meghan Hoffman <meghanhoffman@Meghans-MacBook-Pro-2.local>
2025-04-02 14:14:07 -0400

    wiritng js

--- js/video.js
+++ js/video.js
@@ -5,7 +5,125 @@ window.addEventListener("load", function() {
 
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+video = document.getElementById('player1');
+// Page load
+// Initialize the video element and turn off autoplay and turn off looping.
 
+
+
+// Play the video and update the volume information. 
+
+playButton = document.getElementById('play')
+playButton.addEventListener('click', function(){
+
+	console.log('getting play button')
+
+	video.play()
+
+})
+
+// Pause the video.
+
+pauseButton = document.getElementById('pause')
+pauseButton.addEventListener('click', function(){
+
+	console.log('getting pause button')
+
+	video.pause()
+})
+
+// Slow the current video speed by 10% each time the button is clicked and log the new speed to the console.  
+
+slowerButton = document.getElementById('slower')
+slowerButton.addEventListener('click', function(){
+
+	console.log('getting slow down button')
+
+	tenPercent = video.defaultPlaybackRate * .1
+	video.playbackRate = video.playbackRate - tenPercent
+	console.log(video.playbackRate)
+
+})
+
+
+// Increase the current video speed each time the button is clicked and log the new speed to the console.  
+// Change it by an amount proportional to the slow down. CHECK THIS!!  If you slow down three times and 
+// then speed up three times you should be within 5 digits of 100% again.
+
+fasterButton = document.getElementById('faster')
+fasterButton.addEventListener('click', function(){
+
+	console.log('getting speed up button')
+
+	tenPercent = video.defaultPlaybackRate * .1
+	video.playbackRate = video.playbackRate + tenPercent
+	console.log(video.playbackRate)
+
+})
+// Advance the current video by 10 seconds.  If the video length has been exceeded go back to the start 
+// of the video - no farther.   Log the current location of the video.
+
+
+skipButton = document.getElementById('skip')
+skipButton.addEventListener('click', function(){
+
+	console.log('getting skip ahead button')
+
+	console.log(video.currentTime)
+	if ((video.currentTime + 10) < video.seekable.end(0)){
+		video.currentTime = video.currentTime + 10
+	} else {
+		video.currentTime = video.seekable.start(0)
+	}
+
+})
+
+// Mute/unmute the video and update the text in the button.
+muteButton = document.getElementById('mute')
+muteButton.addEventListener('click', function(){
+
+	console.log('getting mute button')
+
+	if (video.volume == 0){
+		video.volume = (document.getElementById('slider').value / 100)
+	} else {
+		video.volume = 0
+	}
+
+})
+
+
+// Change the volume based on the slider and update the volume information.
+volumeSlider = document.getElementById('slider')
+volumeSlider.addEventListener('change', function(){
+
+	console.log('getting slider move')
+
+	video.volume = (volumeSlider.value / 100)
+	console.log(video.volume)
+	document.getElementById('volume').innerHTML = volumeSlider.value + '%'
+
+})
+
+
+// Utilize the existing oldSchool class on the video element
+oldButton = document.getElementById('vintage')
+oldButton.addEventListener('click', function(){
+
+	console.log('getting old button')
+
+	video.classList.add('oldSchool')
+
+})
+
+
+// Remove the oldSchool class from the video.
+
+newButton = document.getElementById('orig')
+newButton.addEventListener('click', function(){
+
+	console.log('getting original button')
+
+	video.classList.remove('oldSchool')
+
+})
